ba546a7076126072f01e209f721cbf2c8d65d474
Francesco Ansanelli <francians@gmail.com>
2020-03-22 15:10:52 +0100

    genero le categorie

--- categories.py
+++ categories.py
@@ -25,8 +25,19 @@ import subprocess
 
 
 CATEGORIES_MAP = {
-    'ALIMENTARI': [ 'shop=convenience', 'shop=general' ],
-    'BAR': [ 'amenity=cafe', 'amenity=bar' ],
+    'ABBIGLIAMENTO': [ 'shop=clothes', 'shop=shoes' ],
+    'ALIMENTARI': [ 'shop=convenience', 'shop=deli', 'shop=frozen_food', 'shop=general', 'shop=health_food', 'shop=seafood', 'shop=supermarket' ],
+    'ANIMALI': [ 'shop=pet' ],
+    'BAR': [ 'amenity=cafe', 'amenity=bar', 'shop=wine' ],
+    'CIBO': [ 'amenity=fast_food', 'amenity=pub', 'amenity=restaurant' ],
+    'COMPUTER_ELETTRONICA': [ 'shop=electronics', 'shop=ink', 'shop=printer_ink' ],
+    'DOLCI': [ 'shop=confectionery', 'shop=pastry' ],
+    'FIORI': [ 'shop=florist', 'shop=garden_centre' ],
+    'LATTE_DERIVATI': [ 'amenity=ice_cream', 'shop=cheese', 'shop=dairy', 'shop=ice_cream' ],
+    'MACELLERIE': [ 'shop=butcher' ],
+    'ORTOFRUTTA': [ 'shop=farm', 'shop=greengrocer' ],
+    'PANETTERIE': [ 'shop=bakery' ],
+    'SALUTE': [ 'amenity=pharmacy', 'shop=hearing_aids', 'shop=herbalist', 'shop=medical_supply' ]
 }
 
 
@@ -36,13 +47,25 @@ def main():
         sys.exit(0)
 
     for key, value in CATEGORIES_MAP.items():
-        f = open(key + '.osm', 'w')
+        f = open('extracted/' + key + '.osm', 'w')
         keep = ' or '.join(value)
         subprocess.run(['osmfilter', './delivery.osm', '--keep=' + keep], stdout=f)
         f.close()
 
-    drop = ' and '.join(CATEGORIES_MAP['ALIMENTARI'] + CATEGORIES_MAP['BAR'])
-    f = open('ALTRI.osm', 'w')
+    drop = ' or '.join(CATEGORIES_MAP['ABBIGLIAMENTO']
+        + CATEGORIES_MAP['ALIMENTARI']
+        + CATEGORIES_MAP['ANIMALI']
+        + CATEGORIES_MAP['BAR']
+        + CATEGORIES_MAP['CIBO']
+        + CATEGORIES_MAP['COMPUTER_ELETTRONICA']
+        + CATEGORIES_MAP['DOLCI']
+        + CATEGORIES_MAP['FIORI']
+        + CATEGORIES_MAP['LATTE_DERIVATI']
+        + CATEGORIES_MAP['MACELLERIE']
+        + CATEGORIES_MAP['ORTOFRUTTA']
+        + CATEGORIES_MAP['PANETTERIE']
+        + CATEGORIES_MAP['SALUTE'])
+    f = open('extracted/ALTRI.osm', 'w')
     subprocess.run(['osmfilter', './delivery.osm', '--drop=' + drop], stdout=f)
 
 
